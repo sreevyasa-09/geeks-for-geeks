6b71363dc123b152958f64a4ca76e4a80ff39c6e
sreevyasa-09 <sreevyasa119@gmail.com>
2026-02-25 07:05:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        // Code Here
+        int n = arr.length, count = 0, length = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i = 0; i < n; i++){
+            if(arr[i] <= k) count--;
+            else count++;
+            
+            if(count > 0) length = Math.max(length, i + 1);
+            if(map.containsKey(count - 1)) length = Math.max(length, i - map.get(count - 1));
+            
+            if(!map.containsKey(count)) map.put(count, i);
+        }
+        return length;
+    }
+}
