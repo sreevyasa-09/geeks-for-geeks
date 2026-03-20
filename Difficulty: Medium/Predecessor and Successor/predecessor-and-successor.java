9d86dbdd66675c17b5d8abdc5b93bccd26dce5e7
sreevyasa-09 <sreevyasa119@gmail.com>
2026-03-20 10:13:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
@@ -0,0 +1,53 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        // code here
+        
+        ArrayList<Node> ansList = new ArrayList<>();
+        ansList.add(null);
+        ansList.add(null);
+        
+        Node curr = root;
+        while(curr != null){
+            if(curr.data > key){
+                ansList.set(1 , curr);
+                curr = curr.left;
+            }else if(curr.data < key){
+                ansList.set(0 , curr);
+                curr = curr.right;
+            }else{
+                //find predecessor ---> max in the left subtree
+                if(curr.left != null){
+                    Node temp = curr.left;
+                    while(temp.right != null){
+                        temp = temp.right;
+                    }
+                    ansList.set(0 , temp);
+                }
+                //find successor-->min in the right subtree
+                if(curr.right != null){
+                    Node temp = curr.right;
+                    while(temp.left != null){
+                        temp = temp.left;
+                    }
+                    ansList.set(1 , temp);
+                }
+                break;
+            }
+        }
+        
+        return ansList;
+    }
+}
