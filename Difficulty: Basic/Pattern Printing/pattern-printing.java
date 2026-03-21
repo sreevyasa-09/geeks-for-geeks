53b7d3cb7376adc80bfd194aab5f31eadc2e9dac
sreevyasa-09 <sreevyasa119@gmail.com>
2026-03-21 13:32:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern Printing/pattern-printing.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    static void printPattern(int n) {
+        // code here
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.print(" ");
+        }
+    }
+}
