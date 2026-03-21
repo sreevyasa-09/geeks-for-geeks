8461e70e16daab1b029480c61bf88317d2836c8e
sreevyasa-09 <sreevyasa119@gmail.com>
2026-03-21 13:12:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,14 +1,14 @@
 class Solution {
     int missingNum(int arr[]) {
         // code here
-        int n = arr.length;
-        int key = 0;
-        for(int i = 1; i <= (n+1); i++){
-            key = key^i;
-            if(i <= n){
-                key = key^arr[i - 1];
-            }
+        int n = arr.length+1;
+        int[] hash = new int[n + 1];
+        for(int i = 0; i < n-1; i++){
+            hash[arr[i]]++;
         }
-        return key;
+        for(int i = 1; i <= n; i++){
+            if(hash[i] == 0) return i;
+        }
+        return -1;
     }
 }
